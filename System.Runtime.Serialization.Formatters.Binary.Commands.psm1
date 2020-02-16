0acec9583f5c26b190b27d3fe2ce133e3900542b
lmissel <39304706+lmissel@users.noreply.github.com>
2020-02-16 13:57:48 +0100

    Update System.Runtime.Serialization.Formatters.Binary.Commands.psm1

--- System.Runtime.Serialization.Formatters.Binary.Commands.psm1
+++ System.Runtime.Serialization.Formatters.Binary.Commands.psm1
@@ -56,6 +56,9 @@ function ConvertTo-BinaryFile
         [String] $File,
 
         # Hilfebeschreibung zu Param2
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=1)]
         [System.Object]
         $InputObject
     )
